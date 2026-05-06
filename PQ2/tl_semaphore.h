5b0d9244d5d3337f32df97d41f0e1c6e57e0135c
Nathaniel Kinzelberg <nathanielkinzelberg@gmail.com>
2026-05-06 16:25:15 +0300

    Skeleton for tl_semaphore.h

---
+++ PQ2/tl_semaphore.h
@@ -0,0 +1,118 @@
+#ifndef TL_SEMAPHORE_H
+#define TL_SEMAPHORE_H
+
+/* =========================
+   REQUIRED INCLUDES
+   ========================= */
+
+#include <stdatomic.h>
+
+
+/* =========================
+   TICKET LOCK
+   ========================= */
+
+/*
+    IMPLEMENT:
+    - atomic ticket counter
+    - atomic current ticket
+*/
+typedef struct
+{
+
+
+
+
+} ticket_lock;
+
+
+/*
+    IMPLEMENT IN tl_semaphore.c:
+
+    initialize:
+    - ticket
+    - cur_ticket
+*/
+void ticketlock_init(ticket_lock* lock);
+
+
+/*
+    IMPLEMENT IN tl_semaphore.c:
+
+    1. take a ticket using atomic_fetch_add
+    2. wait until current ticket == my ticket
+    3. while waiting:
+            sched_yield()
+*/
+void ticketlock_acquire(ticket_lock* lock);
+
+
+/*
+    IMPLEMENT IN tl_semaphore.c:
+
+    increment current ticket
+*/
+void ticketlock_release(ticket_lock* lock);
+
+
+
+/* =========================
+   SEMAPHORE
+   ========================= */
+
+/*
+    IMPLEMENT:
+    - integer value
+    - internal ticket lock
+*/
+typedef struct
+{
+
+
+
+
+} semaphore;
+
+
+/*
+    IMPLEMENT IN tl_semaphore.c:
+
+    initialize:
+    - semaphore value
+    - internal ticket lock
+*/
+void semaphore_init(semaphore* sem, int initial_value);
+
+
+/*
+    IMPLEMENT IN tl_semaphore.c:
+
+    LOOP FOREVER:
+
+        acquire internal ticket lock
+
+        if value > 0:
+            decrement value
+            release lock
+            return
+
+        release lock
+
+        sched_yield()
+*/
+void semaphore_wait(semaphore* sem);
+
+
+/*
+    IMPLEMENT IN tl_semaphore.c:
+
+    acquire internal lock
+
+    increment value
+
+    release lock
+*/
+void semaphore_signal(semaphore* sem);
+
+
+#endif
